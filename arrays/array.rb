7ada73732d514500664aa30c4d64e8f7da50fceb
g00darup <good.arup@gmail.com>
2021-03-09 23:17:39 -0800

    Update array.rb

--- arrays/array.rb
+++ arrays/array.rb
@@ -31,3 +31,33 @@ arr = nums.map{|a| a**2}
 puts arr.select {|a| a > 3}
 nums.each {|a| print a, " "}
 arr.each {|a| print a, " "}
+
+def find_index(arr, num)
+  	return arr.index(num)
+end
+
+nums = [1,2,3,4,5]
+puts find_index(nums,3)
+
+def find_length(arr)
+  return arr.length
+end
+
+def replace_element(arr,index,val)
+  return arr[index]=val
+end
+
+def even_elements(arr)
+	return arr.select{|num| num.even?}.select{|x| x<7}
+end
+
+nums = [1,2,3,4,5,6,7,8]
+puts find_length(nums)
+print replace_element(nums,3,0)
+puts even_elements(nums)
+
+result = nums.slice(0, 5) + nums.slice(-4, 4)
+#result = numbers.select { |number| number != 6 }
+#result = numbers.reject { |number| number == 6 }
+
+p result
